1d976f502f6d6a872a771677252ffa6e9fe05bee
polinamusheva <polimusheva@gmail.com>
2018-03-10 19:39:40 +0200

    problem 01.Max Sequense of Equal Elements in Lists Exercise edited

--- 06.EX_08.02_Lists_EX/Lists_Exercise_Solutions/01.Max Sequence of Equal Elements.cs
+++ 06.EX_08.02_Lists_EX/Lists_Exercise_Solutions/01.Max Sequence of Equal Elements.cs
@@ -2,30 +2,65 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace EX_LISTS
+namespace EX_08._02_Lists_NEW
 {
     class Program
     {
         static void Main(string[] args)
         {
-            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            List<int> numbers = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
 
-            List<int> sorted = numbers;
-
-            int sum = 1;
-            int sumMax = 0;
-
-           sorted.Sort();
-
-            Console.WriteLine(string.Join(" ",numbers));
+            int count = 1;
+            int countMax = 1;
+            int valueIndexMax = numbers[0];
 
 
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
 
 
+                if (numbers[i] == numbers[i + 1])
+                {
+                    count++;
+                }
+                else
+                {
+                    if (count > countMax)
+                    {
+                        countMax = count;
+                        valueIndexMax = numbers[i];
+                    }
+
+                    count = 1;
+                }
 
 
-            //numbers.RemoveAll(n=>n==0)
+            }
+
+            if (count <= countMax)
+            {
+                //string result = new string((char)(numbers[numbers.Count - 1]), count);
+
+                // List<int> result = new List<int>();
+
+                for (int i = 0; i < countMax; i++)
+                {
+                    Console.Write($"{valueIndexMax} ");
+                }
+                Console.WriteLine();
+            }
+
+
+            else
+            {
+
+                for (int i = 0; i < count; i++)
+                {
+                    Console.Write($"{numbers[numbers.Count - 1]} ");
+                }
+                Console.WriteLine();
+            }
+
         }
     }
-    
 }
